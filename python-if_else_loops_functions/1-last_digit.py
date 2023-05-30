70176b995d185babe50af411cea5f33a01c84603
EvaMicich <6366@holbertonstudents.com>
2023-05-29 22:24:44 -0700

    fixed line length

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number - (10 * int(number / 10))
-if last_digit == 0:
+last_dgt = number - (10 * int(number / 10))
+if last_dgt == 0:
     print(f"Last digit of {number} is 0 and is 0")
-elif last_digit < 6:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
-elif last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+elif last_dgt < 6:
+    print(f"Last digit of {number} is {last_dgt} and is less than 6 and not 0")
+elif last_dgt > 5:
+    print(f"Last digit of {number} is {last_dgt} and is greater than 5")
