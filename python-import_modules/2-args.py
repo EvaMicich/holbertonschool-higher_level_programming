3e1af4039f3f9b83e4aea35b48ae59fe28a432f1
EvaMicich <6366@holbertonstudents.com>
2023-06-02 04:41:50 -0700

    name and main added

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print("0 arguments.")
-if len(sys.argv) == 2:
-    print("1 argument:\n1: {}".format(sys.argv[1]))
-if len(sys.argv) > 2:
-    print("{} arguments:".format(len(sys.argv)-1))
-    i = 1
-    for i in range(len(sys.argv)):
-        if i == 0:
-            continue
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    if len(sys.argv) == 2:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
+    if len(sys.argv) > 2:
+        print("{} arguments:".format(len(sys.argv)-1))
+        for i in range(len(sys.argv)):
+            if i == 0:
+                continue
+            print("{}: {}".format(i, sys.argv[i]))
