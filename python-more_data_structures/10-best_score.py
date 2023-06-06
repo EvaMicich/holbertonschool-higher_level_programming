b4248316f709a760dee78a39233eb806eb48648b
EvaMicich <6366@holbertonstudents.com>
2023-06-06 04:03:29 -0700

    new method

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,4 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    return max(a_dictionary)
+    for i in a_dictionary:
+        if a_dictionary[i] == max(a_dictionary.values()):
+            return i
