8ac3e6be23cc407931e7b51aa9d5e70b270e8dd0
EvaMicich <6366@holbertonstudents.com>
2023-06-08 00:29:07 -0700

    pycodestyle line count

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,13 @@ def roman_to_int(roman_string):
     if roman_string is None:
         return 0
     total = 0
-    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     roman_list = list(roman_string)
     prev_char = None
     for char in roman_list:
-        if prev_char and roman_dict[char] > roman_dict[prev_char]:
-            total += roman_dict[char] - 2 * roman_dict[prev_char]
+        if prev_char and roman_d[char] > roman_d[prev_char]:
+            total += roman_d[char] - 2 * roman_d[prev_char]
         else:
-            total += roman_dict[char]
+            total += roman_d[char]
         prev_char = char
     return total
