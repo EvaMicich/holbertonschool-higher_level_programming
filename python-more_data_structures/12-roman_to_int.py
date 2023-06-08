a379dae5707d43d91cb6a7d39ce5f8f51f6c51b6
EvaMicich <6366@holbertonstudents.com>
2023-06-08 00:25:13 -0700

    update after internet consultation

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,27 +3,13 @@ def roman_to_int(roman_string):
     if roman_string is None:
         return 0
     total = 0
-    for i in range(len(roman_string)):
-        if roman_string[i] == "I":
-            total += 1
-        if roman_string[i] == "V":
-            if i > 0 and roman_string[i - 1] == "I":
-                total -= 2
-            total += 5
-        if roman_string[i] == "X":
-            if i > 0 and roman_string[i - 1] == "I":
-                total -= 2
-            total += 10
-        if roman_string[i] == "L":
-            if i > 0 and roman_string[i - 1] == "X":
-                total -= 20
-            total += 50
-        if roman_string[i] == "C":
-            if i > 0 and roman_string[i - 1] == "X":
-                total -= 20
-            total += 100
-        if roman_string[i] == "D":
-            if i > 0 and roman_string[i - 1] == "C":
-                total -= 200
-            total += 500
+    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_list = list(roman_string)
+    prev_char = None
+    for char in roman_list:
+        if prev_char and roman_dict[char] > roman_dict[prev_char]:
+            total += roman_dict[char] - 2 * roman_dict[prev_char]
+        else:
+            total += roman_dict[char]
+        prev_char = char
     return total
