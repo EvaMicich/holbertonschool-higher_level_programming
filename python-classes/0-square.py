645eaeb3ed1750cb3a54a27ffb46d453fa0eaf51
EvaMicich <6366@holbertonstudents.com>
2023-06-11 21:57:22 -0700

    added more comments

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Square class example"""
+"""
+Square class example
+"""
 
 class Square:
-    """The simplest examples of a class"""
+    """
+    The simplest examples of a class, pass only
+    """
     pass
