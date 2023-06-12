54c6aa6dcaaaa6b45c94c466a31f91ba8d120eb3
EvaMicich <6366@holbertonstudents.com>
2023-06-11 22:28:50 -0700

    added size private variable

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+1 #!/usr/bin/python3
+"""
+Square class example
+"""
+
+
+class Square:
+    """
+    A square initialised with a size as private
+    """
+    def __init__(self, __size):
+        self.__size = __size
