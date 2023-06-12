91c8d36d5c4eb3bbbe2caf3091e28d7eeaa1801a
EvaMicich <6366@holbertonstudents.com>
2023-06-11 22:45:14 -0700

    code to add size to square class

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Square class example
+"""
+
+
+class Square:
+    """
+    A square initialised with a size as private, error messages incl
+    """
+    def __init__(self, size=0):
+        self.__size = __size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
