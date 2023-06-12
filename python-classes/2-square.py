ea44080721ecd1735c80df2623d407f81fa3e546
EvaMicich <6366@holbertonstudents.com>
2023-06-11 22:48:21 -0700

    square class with errors

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     A square initialised with a size as private, error messages incl
     """
     def __init__(self, size=0):
-        self.__size = __size
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
