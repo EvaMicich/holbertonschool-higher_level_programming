ca7de905462821a72fd1e9597e623c202be3ce61
EvaMicich <6366@holbertonstudents.com>
2023-06-14 22:05:57 -0700

    text file to test code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """
+    adds to integer
+    """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     elif type(b) != int and type(b) != float:
