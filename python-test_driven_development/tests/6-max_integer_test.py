0af04cd565504b76c4e51aca143abc8c9fcbcbb5
EvaMicich <6366@holbertonstudents.com>
2023-06-16 02:00:29 -0700

    test for list max at different locations

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,3 +5,8 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    def test_max_int(self):
+        #test maximum value in list
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([100, 2, 3, 4]), 100)
+        self.assertAlmostEqual(max_integer([1, 2, 300, 4]), 300)
