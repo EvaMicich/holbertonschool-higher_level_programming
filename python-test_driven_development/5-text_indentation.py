eccbd252d1eae431506289099df5830a69e53ffd
EvaMicich <6366@holbertonstudents.com>
2023-06-15 23:46:50 -0700

    adding blank lines

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 5-text_indentation
+text printed with 2 new lines after chars .?:
+"""
+
+
+def text_indentation(text):
+    """
+    indents text
+
+    argument:
+    text: text to be printed with 2 new lines after chars . ? :
+
+    return:
+    void
+    """
+    pass
