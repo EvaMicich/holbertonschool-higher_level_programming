6757d8cd0337ab8799e3a179ab332077a24748c4
EvaMicich <6366@holbertonstudents.com>
2023-06-16 00:03:06 -0700

    added test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,4 +15,18 @@ def text_indentation(text):
     return:
     void
     """
-    pass
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    text_update = str(text)
+    after_new_line = False
+    for c in text_update:
+        if after_new_line:
+            if c == " ":
+                continue
+            after_new_line = False
+        if c == '.' or c == '?' or c == ':':
+            print("")
+            print("")
+            after_new_line = True
+        else:
+            print(c, end="")
