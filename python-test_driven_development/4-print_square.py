88706930d099e2e0ccea2f0efb8caee0328b295b
EvaMicich <6366@holbertonstudents.com>
2023-06-15 23:26:47 -0700

    printing square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module: 4-print_square
+"""
+
+
+def print_square(size):
+    """
+    prints a square using # character of size (size)
+
+    argument:
+    size: size of square
+
+    return:
+    void
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
