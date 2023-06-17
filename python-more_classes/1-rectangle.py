9fe0b19a05100adc0d61c95b13f3e813cff522f3
EvaMicich <6366@holbertonstudents.com>
2023-06-16 20:18:12 -0700

    set up rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Rectangle class example
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class with width and height
+    """
+
+
+    def __init__(self, width=0, height=0):
+        """
+        initstantiation of rectangle
+
+        arguments:
+        width
+        height
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """get width of rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width of rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get height of rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height of rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
