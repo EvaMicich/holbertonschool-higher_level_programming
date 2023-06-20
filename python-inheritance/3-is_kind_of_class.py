f090df49a61b5ff89337ad3e003ac329bd3bbdb8
EvaMicich <6366@holbertonstudents.com>
2023-06-19 22:50:44 -0700

    is kind of class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module tests for object in a class"""
+
+
+def is_same_class(obj, a_class):
+    """method to check if object is in a_class)"""
+    return isinstance(obj, a_class)
