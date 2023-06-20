620a5cdf81ca37cd921a11613c239d182859227f
EvaMicich <6366@holbertonstudents.com>
2023-06-20 04:21:31 -0700

    area method added

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module to clreate class BaseGeometry"""
+
+
+class BaseGeometry():
+    """new basegeometry class"""
+
+    def area(self):
+        """area of the geometry"""
+        raise Exception("area() is not implemented")
