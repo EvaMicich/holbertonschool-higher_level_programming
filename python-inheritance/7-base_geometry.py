9ca9a8d2e00905c26a5c5730b92f68ff2fab70b2
EvaMicich <6366@holbertonstudents.com>
2023-06-20 04:47:15 -0700

    tests and integer test

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module to clreate class BaseGeometry"""
+
+
+class BaseGeometry():
+    """new basegeometry class"""
+
+    def area(self):
+        """area of the geometry"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates integer value"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
