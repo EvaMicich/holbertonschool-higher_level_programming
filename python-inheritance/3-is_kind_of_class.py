1c26dbffee33b491006dcb3c3e9dd2037c9bdc2c
EvaMicich <6366@holbertonstudents.com>
2023-06-19 22:55:58 -0700

    fix method name

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,11 @@
 """module tests for object in a class"""
 
 
-def is_same_class(obj, a_class):
-    """method to check if object is in a_class)"""
+def is_kind_of_class(obj, a_class):
+    """
+    method to check if object is in a_class or inherited
+
+    obj: object
+    a_class: class to check
+    """
     return isinstance(obj, a_class)
