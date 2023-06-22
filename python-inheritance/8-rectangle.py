0370641478934da02b6f3749121921902302609b
EvaMicich <6366@holbertonstudents.com>
2023-06-21 20:30:14 -0700

    added lines for pep8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,6 +5,7 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 bg = BaseGeometry()
 
+
 class Rectangle(BaseGeometry):
     """rectangle class inherits BaseGeometry"""
     def __init__(self, width, height):
