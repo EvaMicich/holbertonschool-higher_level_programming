b489a966c8e65cf61d6b247f3aeaacc15cfe10e7
EvaMicich <6366@holbertonstudents.com>
2023-06-22 00:04:19 -0700

    same as qn 10

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""module for rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+bg = BaseGeometry()
+
+
+class Rectangle(BaseGeometry):
+    """rectangle class inherits BaseGeometry"""
+    def __init__(self, width, height):
+        bg.integer_validator("width", width)
+        bg.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        return self.__width * self.__height
+
+
+class Square(Rectangle):
+    """square class inherits from rectangle class"""
+    def __init__(self, size):
+        bg.integer_validator("size", size)
+        self.__width = size
+        self.__height = size
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        return self.__width * self.__height
