3b9393ef57b4a21e4b0595f2a4d3de96e7a253fa
EvaMicich <6366@holbertonstudents.com>
2023-06-22 21:06:55 -0700

    try new syntax

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, encoding="utf-8") as myFile:
-        print(myFile.read())
+    with open(filename, encoding="utf-8") as my_file:
+        read_file = my_file.read()
+        print(read_file, end="")
