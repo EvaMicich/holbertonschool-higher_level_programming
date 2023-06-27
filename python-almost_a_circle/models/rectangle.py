a486f68ca2f5d4a07563ea1340bd5997ec8028a1
EvaMicich <6366@holbertonstudents.com>
2023-06-27 00:16:38 -0700

    added error checks

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,10 +7,10 @@ class Rectangle(Base):
     """rectangle class inheriting from base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """instatiate the rectangle"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
@@ -21,6 +21,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """set width of rectangle"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -31,6 +35,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """set height of rectangle"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     @property
@@ -41,6 +49,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """set x of rectangle"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -51,4 +63,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """set y of rectangle"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
