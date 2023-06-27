6e46c15c9f6f975e4f35edb93f0130f86e61f59a
EvaMicich <6366@holbertonstudents.com>
2023-06-27 04:57:45 -0700

    fix formating

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,4 +87,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """returns string describing the rectangle"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - \
+{self.width}/{self.height}"
