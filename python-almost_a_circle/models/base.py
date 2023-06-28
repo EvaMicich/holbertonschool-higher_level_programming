69b660fb47c4dfd3ed98cb511a079bc22eba7a09
EvaMicich <6366@holbertonstudents.com>
2023-06-27 23:51:50 -0700

    json conversaion

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """bass class"""
+import json
 
 
 class Base():
@@ -13,3 +14,10 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns list on dictionaries in json format"""
+        if list_dictionaries is None:
+            return f"[]"
+        else:
+            return json.dumps(list_dictionaries)
