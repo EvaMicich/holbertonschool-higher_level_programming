21a041670eb46bc8d759508257144cb28c85fa83
EvaMicich <6366@holbertonstudents.com>
2023-06-27 22:28:36 -0700

    added size atrribute

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,6 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """instantiate square object"""
         super().__init__(size, size, x, y, id)
+        self.__size = size
 
     def __str__(self):
         """returns string describing the rectangle"""
@@ -15,3 +16,18 @@ class Square(Rectangle):
             f"[Square] ({self.id}) {self.x}/{self.y} - "
             f"{self.height}"
             )
+
+    @property
+    def size(self):
+        """get size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set size of square"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
