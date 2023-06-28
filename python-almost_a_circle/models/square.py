fec18be5c3b4e57851f079dbcd171d4ba61d5999
EvaMicich <6366@holbertonstudents.com>
2023-06-27 22:06:36 -0700

    sqaure from a rectangle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" square module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ square class inherits from retangle class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """instantiate square object"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """returns string describing the rectangle"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - \
+{self.height}"
