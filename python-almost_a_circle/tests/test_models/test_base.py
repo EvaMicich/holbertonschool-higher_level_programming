1b0474255c168c6db7a4dcf49ee1843f1c7b100d
EvaMicich <6366@holbertonstudents.com>
2023-06-28 23:26:59 -0700

    tests

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,26 @@
+"""unittest for base module"""
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    """test class Base"""
+    def test_id(self):
+        """Base assigns unique id"""
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertNotEqual(b1.id, b2.id)
+
+    def test_id_plus_one(self):
+        """Base assigns sequential id"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id + 1, b2.id)
+        self.assertEqual(b2.id + 1, b3.id)
+
+    def test_id_set(self):
+        """Base assigns set id"""
+        b1 = Base(89)
+        self.assertEqual(b1.id, 89)
