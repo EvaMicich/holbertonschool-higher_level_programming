095b24951f90e207d5af568298de93531808fcb4
EvaMicich <6366@holbertonstudents.com>
2023-06-29 18:52:09 -0700

    test added

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -19,3 +19,13 @@ class TestBase(unittest.TestCase):
         b3 = Base()
         self.assertEqual(b1.id + 1, b2.id)
         self.assertEqual(b2.id + 1, b3.id)
+
+    def test_saving_id(self):
+        """Test that checks id is correctly assigned"""
+        b1 = Base(89)
+        self.assertEqual(b1.id, 89)
+
+    def test_to_json_string(self):
+        """Tests the method to_json_string for different input cases"""
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
