c4439ece21eda252be03b908be0cdcfe68320741
EvaMicich <6366@holbertonstudents.com>
2023-06-29 22:33:18 -0700

    more rect test

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -28,3 +28,18 @@ class TestRectangle(unittest.TestCase):
         r4 = Rectangle(1, 2)
         self.assertEqual(r4.width, 1)
         self.assertEqual(r4.height, 2)
+
+    def test_args(self):
+        """string argument throws error at every argument position"""
+        r1 = Rectangle(1, 2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            r1.width = "1"
+        r2 = Rectangle(1, 2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            r2.height = "2"
+        r3 = Rectangle(1, 2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            r3.x = "3"
+        r4 = Rectangle(1, 2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            r4.y = "4"
