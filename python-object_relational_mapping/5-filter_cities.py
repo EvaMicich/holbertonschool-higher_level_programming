267e3936b423016305458115b4e5ef0695b2c76e
EvaMicich <6366@holbertonstudents.com>
2023-07-31 22:15:46 -0700

    filter state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states
+table that match the argument"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+    FROM cities\
+    JOIN states ON cities.state_id = states.id\
+    WHERE states.name LIKE %s\
+    ORDER BY cities.id;", (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
